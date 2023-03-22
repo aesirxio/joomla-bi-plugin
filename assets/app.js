112aac5b6b077e0bd77a9564239972c2036b4a57
Lu Nguyen <lu@r-digital.tech>
2023-03-22 18:06:15 +0700

    Fix

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,10 @@
+import 'aesirx-bi-app/dist/index.css';
+import 'aesirx-bi-app/dist/app.css';
 import './app.scss';
+import { BiIntegration } from 'aesirx-bi-app';
 
 const BIApp = () => {
-  return <></>;
+  return <BiIntegration />;
 };
 
 export default BIApp;
